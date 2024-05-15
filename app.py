052ef741b9554280feb38fcaffba253d1ade27d1
dartiros <realdarter1234@gmail.com>
2024-05-14 23:20:35 -0700

    Fixed application bug
    Fixed application bug

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from main import *
 
 app = Flask(__name__)
 
@@ -8,12 +8,13 @@ def home():
     return render_template('index.html')
 
 @app.route('/generate_response', methods=['POST'])
-def generate_response():
+def generate_a_response():
     user_input = request.form['user_input']
-    _, response = generate_response(user_input) 
+    _, response = generate_response(str(user_input)) 
+    print(response)
     return render_template('response.html', response=response)
 
 
 if __name__ == '__main__':
-    from main import *
-    app.run(debug=True)
+    print("done going to application now.")
+    app.run(debug=False)
